0371f145786a9f9eb62500b35edcdb4b49110cf7
eshansri7-u <eshansri7@gmail.com>
2026-03-13 13:58:03 -0500

    Added solution code

---
+++ 1_twosum.py
@@ -0,0 +1,11 @@
+def twoSum(self, nums, target):
+        indices = {}
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
---
+++ 49_groupanagrams.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+def groupAnagrams(self, strs):
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
